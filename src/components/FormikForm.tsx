b1a9e588e6adfdda9ad356882b2897a0e4a04c18
thekinita <thekinita@mail.ru>
2024-12-14 03:02:30 +0500

    refactor: update FormikForm

--- src/components/FormikForm.tsx
+++ src/components/FormikForm.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Formik, Form } from 'formik'
-import { Button, Flex, VStack } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  Input,
+  NumberInput,
+  NumberInputField,
+  VStack
+} from '@chakra-ui/react'
 import FormikWrapper from './FormikWrapper'
+import validationSchema from '../utils/validationSchema'
 
 const FormikForm: React.FC = () => (
   <Flex height="100vh" alignItems="center" justifyContent="center">
     <Formik
       initialValues={{
-        delivery: false,
-        eat: null
+        name: '',
+        address: '',
+        floor: 0,
+        totalFloors: 0,
+        square: 0,
+        livingSquare: 0,
+        kitchenSquare: 0
       }}
+      validationSchema={validationSchema}
       onSubmit={(values) => {
         console.log(values)
       }}
@@ -18,18 +32,26 @@ const FormikForm: React.FC = () => (
         <Form>
           <VStack spacing={4}>
             <FormikWrapper
-              name="delivery"
-              type="checkbox"
-              options={[{ label: 'Доставка' }]}
+              name="name"
+              as={Input}
+              placeholder="Название объекта"
             />
-            <FormikWrapper
-              name="eat"
-              type="radio"
-              options={[
-                { value: 'burger', label: 'Бургер' },
-                { value: 'pizza', label: 'Пицца' }
-              ]}
-            />
+            <FormikWrapper name="address" as={Input} placeholder="Адрес" />
+            <FormikWrapper name="floor" as={NumberInput}>
+              <NumberInputField placeholder="Этаж" />
+            </FormikWrapper>
+            <FormikWrapper name="totalFloors" as={NumberInput}>
+              <NumberInputField placeholder="Количество этажей в доме" />
+            </FormikWrapper>
+            <FormikWrapper name="square" as={NumberInput}>
+              <NumberInputField placeholder="Площадь" />
+            </FormikWrapper>
+            <FormikWrapper name="livingSquare" as={NumberInput}>
+              <NumberInputField placeholder="Жилая площадь" />
+            </FormikWrapper>
+            <FormikWrapper name="kitchenSquare" as={NumberInput}>
+              <NumberInputField placeholder="Площадь кухни" />
+            </FormikWrapper>
             <Button type="submit" colorScheme="blue">
               Отправить
             </Button>
